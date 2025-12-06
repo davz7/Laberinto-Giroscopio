f4b4c1ce212ffcd68e527b7a760d438290a4c200
“Anapau-15” <20243ds026@utez.edu.mx>
2025-12-05 18:45:28 -0600

    Se agrega la navegación de la aplicación

--- app/src/main/java/mx/edu/laberinto_giroscopio/ui/nav/AppNavigation.kt
+++ app/src/main/java/mx/edu/laberinto_giroscopio/ui/nav/AppNavigation.kt
@@ -1,2 +1,27 @@
 package mx.edu.laberinto_giroscopio.ui.nav
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import mx.edu.laberinto_giroscopio.ui.screens.GameScreen
+import mx.edu.laberinto_giroscopio.ui.screens.HomeScreen
+import mx.edu.laberinto_giroscopio.ui.screens.LoginScreen
+import mx.edu.laberinto_giroscopio.viewmodel.GameViewModel
+import mx.edu.laberinto_giroscopio.viewmodel.ScoresScreen
+import mx.edu.laberinto_giroscopio.viewmodel.UserViewModel
+
+@Composable
+fun AppNavigation() {
+    val userVM = UserViewModel()
+    val scoreVM = ScoreViewModel()
+    val gameVM = GameViewModel()
+    val nav = rememberNavController()
+    NavHost(navController = nav, startDestination = "login") {
+        composable("login") { LoginScreen(nav, userVM) }
+        composable("home") { HomeScreen(nav) }
+        composable("game") { GameScreen(gameVM) }
+        composable("scores") { ScoresScreen(scoreVM) }
+    }
+
+
